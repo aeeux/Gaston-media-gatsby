0df78f07a803b5520078347ca34e4ba26d4cf77e
lucasjolibois54 <lucasjolibois@icloud.com>
2021-10-27 20:44:22 +0200

    Create aboutHome.js

---
+++ src/components/aboutHome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const aboutHome = () => {
+    return (
+        <div>
+            <h1 className="heading-ab">What we do </h1>
+        </div>
+    )
+}
+
+export default aboutHome
