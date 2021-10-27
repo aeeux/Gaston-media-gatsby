3d9ea0d95ff2ab325174695a8939cecd403e4b43
Daudmir <daud0013@edu.easj.dk>
2021-10-27 17:11:40 +0200

    Create index.js

---
+++ src/components/index/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <h1 className="heading">Creative</h1>
+            <h2 className="heading-a">Agency</h2>
+        </div>
+    )
+}
+
+export default index
