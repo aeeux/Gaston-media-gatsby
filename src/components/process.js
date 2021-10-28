716a9724cc73ce5388b328ace24e035819132bce
lucasjolibois54 <lucasjolibois@icloud.com>
2021-10-28 16:49:37 +0200

    Create process.js

---
+++ src/components/process.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const process = () => {
+    return (
+        <div>
+            <li className="heading-ab">What we do </li>
+
+           <div>
+            
+
+
+            </div>
+        </div>
+    )
+}
+
+export default process
