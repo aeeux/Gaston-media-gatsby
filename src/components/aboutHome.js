ad74b3ccf3717112aaa8bf5c044cbd764e021002
lucasjolibois54 <lucasjolibois@icloud.com>
2021-10-28 23:47:38 +0200

    Update aboutHome.js

--- src/components/aboutHome.js
+++ src/components/aboutHome.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import box from '../images/Rectangle.png';
+import box2 from '../images/creativity.jpeg';
 
 const aboutHome = () => {
     return (
@@ -10,7 +11,7 @@ const aboutHome = () => {
             <h2 className="sub-heading-ab">Creative solutions <span className="sub-h-span">for impact</span></h2></div>
             <div className="center">
             <p className="about-p">We are a creative design and develop agency created to revolutionize the way you look a digital solutions. we’re a team of passioante and hard wokring people here to push your business to the next level. What’s not to like?</p>
-            <img className="img-about" src={box} alt="Logo" />
+            <img className="img-about" src={box2} alt="Logo" />
 
 
             </div>
