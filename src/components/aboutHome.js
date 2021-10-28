a3cf971ef1fd8cd4d471f3c0bc932ea3896725e5
lucasjolibois54 <lucasjolibois@icloud.com>
2021-10-28 15:23:28 +0200

    Update aboutHome.js

--- src/components/aboutHome.js
+++ src/components/aboutHome.js
@@ -3,7 +3,13 @@ import React from 'react'
 const aboutHome = () => {
     return (
         <div>
-            <h2 className="heading-ab">What we do </h2>
+            <li className="heading-ab">What we do </li>
+
+           <div>
+            <h2 className="sub-heading-ab">Creative solutions <span className="sub-h-span">for impact</span></h2></div>
+            <div className="center">
+            <p className="about-p">We are a creative design and develop agency created to revolutionize the way you look a digital solutions. we’re a team of passioante and hard wokring people here to push your business to the next level. What’s not to like?</p>
+            </div>
         </div>
     )
 }
