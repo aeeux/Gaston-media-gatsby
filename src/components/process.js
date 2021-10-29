e9cd7205b596bd1c51bd0ae844dc9cb5af186676
lucasjolibois54 <lucasjolibois@icloud.com>
2021-10-29 20:52:53 +0200

    Update process.js

--- src/components/process.js
+++ src/components/process.js
@@ -6,16 +6,32 @@ const process = () => {
         <div className="font">
             <li className="heading-ab">Work Process </li>
 
-            <div className="overall-div-process">
-                <div className="div-1">
-                    <h2 className="number-pro">01</h2>
-                    <h3 className="title-pro">Research</h3>
-                    <p className="p-pro">We rebrand businesses through a well thoughout analysis on the market, the competitors and the customergroups.</p>
-                </div>
-
-                <div className="div-2">
-                    <img className="img-p" src={processimg} alt="process img" /></div>
-            </div>
+            <div class="timeline">
+  <div class="container1 left">
+    <div class="content">
+      <h2>2017</h2>
+      <p>Lorem ipsum..</p>
+    </div>
+  </div>
+  <div class="container1 right">
+    <div class="content">
+      <h2>2016</h2>
+      <p>Lorem ipsum..</p>
+    </div>
+  </div>
+  <div class="container1 left">
+    <div class="content">
+      <h2>2017</h2>
+      <p>Lorem ipsum..</p>
+    </div>
+  </div>
+  <div class="container1 right">
+    <div class="content">
+      <h2>2016</h2>
+      <p>Lorem ipsum..</p>
+    </div>
+  </div>
+</div>
 
         </div>
     )
