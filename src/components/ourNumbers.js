010ce1ec67a87e202bc73030d1f5e4d0658ff48a
Daudmir <daud0013@edu.easj.dk>
2021-10-30 00:18:04 +0200

    Update ourNumbers.js

--- src/components/ourNumbers.js
+++ src/components/ourNumbers.js
@@ -6,23 +6,24 @@ const ourNumbers = () => {
             <li className="heading-ab">Our numbers </li>
 
             <div>
-            <h2 className="sub-heading-ab sub-heading-abc">Numbers speak <span className="sub-h-span sub-h-span2">louder than words!</span></h2></div>
+                <h2 className="sub-heading-ab sub-heading-abc">Numbers speak <span className="sub-h-span sub-h-span2">louder than words!</span></h2>
+            </div>
             <div className="centering">
-<div className="flexing">
-            <div className="nam nam1">
-                <br/><h2 className="ninenine">99%</h2> <p className="pp">client satisfaction</p>
-            </div>
+                <div className="flexing">
+                    <div className="nam nam1">
+                        <br /><h2 className="ninenine">99%</h2> <p className="pp">client satisfaction</p>
+                    </div>
 
-            <div className="nam nam2">
-                <br/><h2 className="ninenine">>7 years</h2> <p className="pp">over 2 years of expertise</p>
-            </div>
+                    <div className="nam nam2">
+                        <br /><h2 className="ninenine">>7 years</h2> <p className="pp">over 2 years of expertise</p>
+                    </div>
 
-            <div className="nam nam3">
-                <br/><h2 className="ninenine">24/7</h2> <p className="pp">daily support</p>
+                    <div className="nam nam3">
+                        <br /><h2 className="ninenine">24/7</h2> <p className="pp">daily support</p>
+                    </div>
+                </div>
             </div>
-            </div>
-</div>
-            </div>
+        </div>
     )
 }
 
