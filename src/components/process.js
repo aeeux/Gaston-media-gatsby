c72d2d36124a8e528c6c42f09e946172c9bc425e
Daudmir <daud0013@edu.easj.dk>
2021-10-30 00:53:52 +0200

    Update process.js

--- src/components/process.js
+++ src/components/process.js
@@ -2,33 +2,36 @@ import React from 'react'
 import processimg from '../images/research.png';
 
 const process = () => {
-    return (
-        <div className="font">
-            <li className="heading-ab">Work Process </li>
+  return (
+    <div className="font">
+      <li className="heading-ab">Work Process </li>
 
-            <div class="timeline">
-  <div class="container1 left">
-    <div class="content">
-      <h2>Research</h2>
-      <p>We rebrand businesses through a well thoughout analysis on the market, the competitors and the customergroups.</p>
-    </div>
-  </div>
-  <div class="container1 right">
-    <div class="content">
-      <h2>Design</h2>
-      <p>We rebrand businesses through a well thoughout analysis on the market, the competitors and the customergroups.</p>
-    </div>
-  </div>
-  <div class="container1 left">
-    <div class="content">
-      <h2>Development</h2>
-      <p>We rebrand businesses through a well thoughout analysis on the market, the competitors and the customergroups.</p>
-    </div>
-  </div>
-</div>
-
+      <div class="timeline">
+        <div class="container1 left">
+          <div class="content">
+            <h2 className="number-pro">01</h2>
+            <h3 className="tittle-pro">Research</h3>
+            <p>We rebrand businesses through a well thoughout analysis on the market, the competitors and the customergroups.</p>
+          </div>
         </div>
-    )
+        <div class="container1 right">
+          <div class="content">
+            <h2 className="number-pro">02</h2>
+            <h3 className="tittle-pro">Design</h3>
+            <p>We rebrand businesses through a well thoughout analysis on the market, the competitors and the customergroups.</p>
+          </div>
+        </div>
+        <div class="container1 left">
+          <div class="content">
+            <h2 className="number-pro">03</h2>
+            <h3 className="tittle-pro">Development</h3>
+            <p>We rebrand businesses through a well thoughout analysis on the market, the competitors and the customergroups.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
 export default process
