26789ecc69e02729f5b59292830472ccacf0c0c4
lucasjolibois54 <lucasjolibois@icloud.com>
2021-10-29 21:23:39 +0200

    Create ourNumbers.js

---
+++ src/components/ourNumbers.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ourNumbers = () => {
+    return (
+        <div className="font">
+            <li className="heading-ab">Work Process </li>
+
+            <div class="timeline">
+  <div class="container1 left">
+    <div class="content">
+      <h2>Research</h2>
+      <p>We rebrand businesses through a well thoughout analysis on the market, the competitors and the customergroups.</p>
+    </div>
+  </div>
+  <div class="container1 right">
+    <div class="content">
+      <h2>Design</h2>
+      <p>We rebrand businesses through a well thoughout analysis on the market, the competitors and the customergroups.</p>
+    </div>
+  </div>
+  <div class="container1 left">
+    <div class="content">
+      <h2>Development</h2>
+      <p>We rebrand businesses through a well thoughout analysis on the market, the competitors and the customergroups.</p>
+    </div>
+  </div>
+</div>
+
+        </div>
+    )
+}
+
+export default ourNumbers
