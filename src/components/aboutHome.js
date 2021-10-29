da4544ccc4e582e1b591746822f96ec453c0f216
lucasjolibois54 <lucasjolibois@icloud.com>
2021-10-30 01:20:22 +0200

    Update aboutHome.js

--- src/components/aboutHome.js
+++ src/components/aboutHome.js
@@ -8,7 +8,7 @@ const aboutHome = () => {
             <li className="heading-ab">What we do </li>
 
            <div>
-            <h2 className="sub-heading-ab">Creative solutions <span className="sub-h-span">for impact</span></h2></div>
+            <h2 className="sub-heading-ab sub-heading-abcde">Creative solutions <span className="sub-h-span">for impact</span></h2></div>
             <div className="center">
             <p className="about-p">We are a creative design and develop agency created to revolutionize the way you look a digital solutions. we’re a team of passioante and hard wokring people here to push your business to the next level. What’s not to like?</p>
             <img className="img-about" src={box2} alt="Logo" />
