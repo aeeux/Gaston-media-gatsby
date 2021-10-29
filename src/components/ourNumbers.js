02b68726f91cb141c7a0cdce0ae19b4d6f1cea84
lucasjolibois54 <lucasjolibois@icloud.com>
2021-10-29 22:09:44 +0200

    Update ourNumbers.js

--- src/components/ourNumbers.js
+++ src/components/ourNumbers.js
@@ -2,12 +2,24 @@ import React from 'react';
 
 const ourNumbers = () => {
     return (
-        <div className="font">
+        <div className="font flex-other">
             <li className="heading-ab">Work Process </li>
 
             <div>
             <h2 className="sub-heading-ab sub-heading-abc">Numbers speak <span className="sub-h-span sub-h-span2">louder than words!</span></h2></div>
-            
+<div className="flexing">
+            <div className="nam nam1">
+                <br/><h2 className="ninenine">99%</h2> <p>client satisfaction</p>
+            </div>
+
+            <div className="nam nam2">
+                <br/><h2 className="ninenine">>2years</h2> <p>over 2 years of expertise</p>
+            </div>
+
+            <div className="nam nam3">
+                <br/><h2 className="ninenine">24/7</h2> <p>daily support</p>
+            </div>
+</div>
             </div>
     )
 }
