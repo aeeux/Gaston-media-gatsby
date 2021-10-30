1f718db4957dc91d1a9f6a636fb9f74587bc2a64
lucasjolibois54 <lucasjolibois@icloud.com>
2021-10-30 21:38:07 +0200

    Update peopleLove.js

--- src/components/peopleLove.js
+++ src/components/peopleLove.js
@@ -3,8 +3,27 @@ import React from 'react'
 
 const peopleLove = () => {
     return (
-        <div>
+        <div className="margin-love">
         <li className="heading-ab">The people love us </li>
+
+        <div className="loveit">
+        
+        <div>
+        <div>
+            <h2 className="sub-heading-ab sub-heading-abcde">Why people <span className="sub-h-span"> love us</span></h2></div>
+            <div className="center">
+            <p className="love-p">We are a creative design and develop agency created to revolutionize the way you look a digital solutions. we’re a team of passioante and hard wokring people here to push your business to the next level. What’s not to like?</p></div>
+        
+            </div>
+            <div>
+        <p className="message message1">nanm ns c cdsv vdd cv
+weawc sfa ssa cs e rrt sc s esa f rs rfsa fras f</p>
+<p className="message message2">nanm ns c cdsv vdd cv
+weawc sfa ssa cs e rrt sc s esa f rs rfsa fras f</p>
+<p className="message message3">nanm ns c cdsv vdd cv
+weawc sfa ssa cs e rrt sc s esa f rs rfsa fras f</p>
+</div>
+        </div>
         </div>
     )
 }
