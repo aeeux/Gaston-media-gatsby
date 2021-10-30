aeec7dadff6d83ba069fce55143d4695f53afdfe
lucasjolibois54 <lucasjolibois@icloud.com>
2021-10-30 22:48:35 +0200

    Update peopleLove.js

--- src/components/peopleLove.js
+++ src/components/peopleLove.js
@@ -10,7 +10,7 @@ const peopleLove = () => {
         
         <div>
         <div>
-            <h2 className="sub-heading-ab sub-heading-abcde">Why people <span className="sub-h-span"> love us</span></h2></div>
+            <h2 className="sub-heading-ab sub-heading-abcde">Why people <span className="sub-h-span bold"> love us</span></h2></div>
             <div className="">
             <p className="love-p">We are a creative design and develop agency created to revolutionize the way you look a digital solutions. we’re a team of passioante and hard wokring people here to push your business to the next level. What’s not to like?</p></div>
         
