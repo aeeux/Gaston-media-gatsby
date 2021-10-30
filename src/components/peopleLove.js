e2de03f28bd2e6fe7e8572301ac2ade34a278f8a
lucasjolibois54 <lucasjolibois@icloud.com>
2021-10-30 21:08:32 +0200

    Create peopleLove.js

---
+++ src/components/peopleLove.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const peopleLove = () => {
+    return (
+        <div>
+        <li className="heading-ab">The people love us </li>
+        </div>
+    )
+}
+
+export default peopleLove
