5f78cb65a649395cf7596245daf27317e5c91f79
Casper <casp8249@edu.easj.dk>
2021-11-01 18:58:38 +0100

    Slider - Small Bugs

--- src/components/item.js
+++ src/components/item.js
@@ -4,7 +4,9 @@ export default styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 250px;
-    height: 250px;
-    background-color: #ff00ff;
+    width: 85%;
+    min-height: 85%;
+    height: 85%;
+    background-color: none;
+    border-radius: 15px;
 `;
--- src/components/projectsHome.js
+++ src/components/projectsHome.js
@@ -1,31 +1,32 @@
 import React from "react"
 import processimg from "../images/research.png"
 import Carousel from "react-elastic-carousel"
-import Item from "./Item"
+import Item from "./item"
 import { Link } from "gatsby"
+import rectangle from '../images/Rectangle.png';
 
 const changeFromBreakpoints = [
-  { width: 1, itemToShow: 2 },
-  { width: 500, itemToShow: 5 },
+  { width: 1, itemsToShow: 1 },
+  { width: 500, itemsToShow: 2.5 }
 ]
+
+
 
 const projectsHome = () => {
   return (
     <div className="font">
       <li className="heading-ab">Featured Projects </li>
-      <Carousel breakpoints={changeFromBreakpoints}>
-        <Link to='/item1'>
-          <Item>1</Item>
-        </Link>
-        <Item>2</Item>
-        <Item>3</Item>
-        <Item>4</Item>
-        <Item>5</Item>
-        <Item>6</Item>
+      <Carousel breakPoints={changeFromBreakpoints}>
+        
+          <Item><Link draggable="false" to='/item1'><img draggable="false" className="itemImage" src={rectangle}></img></Link></Item>
+          <Item><Link draggable="false" to='/item2'><img draggable="false" className="itemImage" src={rectangle}></img></Link></Item>
+          <Item><Link draggable="false" to='/item3'><img draggable="false" className="itemImage" src={rectangle}></img></Link></Item>
+          <Item><Link draggable="false" to='/item4'><img draggable="false" className="itemImage" src={rectangle}></img></Link></Item>
+
       </Carousel>
-      <div className="scrollbar"></div>
     </div>
   )
 }
+
 
 export default projectsHome
