7bf9885b56f38ac252159b471e2d91481a69edb0
CReedtz <74539585+CReedtz@users.noreply.github.com>
2021-11-01 11:38:21 +0100

    start-carousel

---
+++ src/components/item.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 250px;
+    height: 250px;
+    background-color: #ff00ff;
+`;
--- src/components/projectsHome.js
+++ src/components/projectsHome.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import processimg from '../images/research.png';
+import React from "react"
+import processimg from "../images/research.png"
+import Carousel from "react-elastic-carousel"
+import Item from "./Item"
+import { Link } from "gatsby"
 
+const changeFromBreakpoints = [
+  { width: 1, itemToShow: 2 },
+  { width: 500, itemToShow: 5 },
+]
 
 const projectsHome = () => {
-    return (
-        <div className="font">
-            <li className="heading-ab">Featured Projects </li>
-
-        </div>
-    )
+  return (
+    <div className="font">
+      <li className="heading-ab">Featured Projects </li>
+      <Carousel breakpoints={changeFromBreakpoints}>
+        <Link to='/item1'>
+          <Item>1</Item>
+        </Link>
+        <Item>2</Item>
+        <Item>3</Item>
+        <Item>4</Item>
+        <Item>5</Item>
+        <Item>6</Item>
+      </Carousel>
+      <div className="scrollbar"></div>
+    </div>
+  )
 }
 
 export default projectsHome
