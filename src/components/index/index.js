87abeeecc214e3aca86ae3137e91c34dbafb2b5a
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-05 19:53:52 +0100

    Update index.js

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { motion } from "framer-motion"
 
 const index = () => {
+
     return (
         <div>
+                <motion.div
+       animate={{
+        scale: [1, 2, 2, 1, 1],
+        rotate: [0, 0, 20, 20, 0],
+        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+      }}>
             <h1 className="heading">Creative</h1>
             <h1 className="heading-a">Agency</h1>
+            </motion.div>
         </div>
     )
 }
