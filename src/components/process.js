387563b1715acd86e401132ce8f0d06a967d6fb4
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-05 18:48:54 +0100

    Update process.js

--- src/components/process.js
+++ src/components/process.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import processimg from '../images/research.png';
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
-const process = () => {
+const Process = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, []);
   return (
     <div className="font xtra">
-      <li className="heading-ab">Work Process </li>
+      <li data-aos="fade-right"  className="heading-ab">Work Process </li>
 
-      <div class="timeline">
-        <div class="container1 left">
+      <div  class="timeline">
+        <div data-aos="fade-right"  class="container1 left">
           <div class="content">
             <h2 className="number-pro">01</h2>
             <h3 className="tittle-pro">Research</h3>
             <p>We rebrand businesses through a well thoughout analysis on the market, the competitors and the customergroups.</p>
           </div>
         </div>
-        <div class="container1 process-right">
+        <div data-aos="fade-left"  class="container1 process-right">
           <div class="content">
             <h2 className="number-pro">02</h2>
             <h3 className="tittle-pro">Design</h3>
             <p> When we design, it's a creative process in which we use our intuition and analytical ability to understand the opportunities and constraints business goals, competitive markets, customer needs, and technologies present, that envision, communicate, and realize practical solutions that meet customer needs and create business value</p>
           </div>
         </div>
-        <div class="container1 prcoess-left">
+        <div data-aos="fade-right"  class="container1 prcoess-left">
           <div class="content">
             <h2 className="number-pro">03</h2>
             <h3 className="tittle-pro">Development</h3>
@@ -35,4 +41,4 @@ Our development process is based on the seven stages of the New Product Developm
   )
 }
 
-export default process
+export default Process
