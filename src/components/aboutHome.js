1b10d04122973d06dab4b01eb1ab78ee136df0b6
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-05 23:14:35 +0100

    Update aboutHome.js

--- src/components/aboutHome.js
+++ src/components/aboutHome.js
@@ -9,14 +9,14 @@ const AboutHome = () => {
         Aos.init({duration:2000});
     }, []);
     return (
-        <div data-aos="zoom-in">
-            <li className="heading-ab">What we do </li>
+        <div >
+            <li data-aos="zoom-in" className="heading-ab">What we do </li>
 
            <div>
-            <h2 className="sub-heading-ab sub-heading-abcde">Creative solutions <span className="sub-h-span">for impact</span></h2></div>
+            <h2 data-aos="zoom-in" className="sub-heading-ab sub-heading-abcde">Creative solutions <span className="sub-h-span">for impact</span></h2></div>
             <div className="center">
-            <p className="about-p">We are a creative design and web development agency founded to revolutionize the way you look at digital solutions. we’re a team of passionate and hard working people here to push your business to the next level. What’s not to like?</p>
-            <img className="img-about" src={box2} alt="Logo" />
+            <p data-aos="zoom-in" className="about-p">We are a creative design and web development agency founded to revolutionize the way you look at digital solutions. we’re a team of passionate and hard working people here to push your business to the next level. What’s not to like?</p>
+            <img data-aos="zoom-in" className="img-about" src={box2} alt="Logo" />
 
 
             </div>
