1d9a4f97e53bd5c0029bc1b4652e006e40899d80
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-05 18:48:57 +0100

    Update peopleLove.js

--- src/components/peopleLove.js
+++ src/components/peopleLove.js
@@ -1,28 +1,34 @@
 import React from 'react'
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
 
-const peopleLove = () => {
+const PeopleLove = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, []);
     return (
         <div className="margin-love">
-        <li className="heading-ab">The people love us </li>
+            <li   data-aos="fade-right" className="heading-ab">The people love us </li>
 
-        <div className="loveit">
-        
-        <div>
-        <div>
-            <h2 className="sub-heading-ab sub-heading-abcde">Why people <span className="sub-h-span bold"> love us</span></h2></div>
-            <div className="">
-            <p className="love-p">We are a creative design and develop agency created to revolutionize the way you look a digital solutions. we’re a team of passioante and hard wokring people here to push your business to the next level. What’s not to like?</p></div>
-        
+            <div  data-aos="fade-right" className="loveit">
+
+                <div data-aos="fade-right" >
+                    <div>
+                        <h2 className="sub-heading-ab sub-heading-abcde">Why people <span className="sub-h-span bold"> love us</span></h2></div>
+                    <div className="">
+                        <p className="love-p">We are a creative design and develop agency created to revolutionize the way you look a digital solutions. we’re a team of passioante and hard wokring people here to push your business to the next level. What’s not to like?</p></div>
+
+                </div>
+                <div data-aos="fade-left" >
+                    <p data-aos="fade-left" className="message message1">Nice people, good communication and an even better digital solution. Thank you for your work!</p>
+                    <p data-aos="fade-left" className="message message2">First time working with these guys, and definetly not last time.</p>
+                    <p data-aos="fade-left" className="message message3">Amazing results, simply amazing. I'm speechless. I'll be back for sure!</p>
+                </div>
             </div>
-            <div>
-        <p className="message message1">Nice people, good communication and an even better digital solution. Thank you for your work!</p>
-<p className="message message2">First time working with these guys, and definetly not last time.</p>
-<p className="message message3">Amazing results, simply amazing. I'm speechless. I'll be back for sure!</p>
-</div>
-        </div>
         </div>
     )
 }
 
-export default peopleLove
+export default PeopleLove
