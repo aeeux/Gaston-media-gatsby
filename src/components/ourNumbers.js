3bb6563ceed940debe8eed87f1f753d40871fd03
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-16 23:31:28 +0200

    Update ourNumbers.js

--- src/components/ourNumbers.js
+++ src/components/ourNumbers.js
@@ -21,7 +21,7 @@ const OurNumbers = () => {
                     </div>
 
                     <div className="nam nam2">
-                        <br /><h2 className="ninenine">>7 years</h2> <p className="pp">over 7 years of expertise</p>
+                        <br /><h2 className="ninenine">&gt;7 years</h2> <p className="pp">over 7 years of expertise</p>
                     </div>
 
                     <div className="nam nam3">
